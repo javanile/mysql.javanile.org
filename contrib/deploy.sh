bd33fea0b7a0e49105165709c05da168acae6dfd
francesco <info.francescobianco@gmail.com>
2023-03-30 19:02:56 +0200

    Deploy

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ ! -d /opt/mysql.javanile.org ] && git clone https://github.com/javanile/mysql.javanile.org
+[ ! -d /opt/mysql.javanile.org ] && git clone https://github.com/javanile/mysql.javanile.org /opt/mysql.javanile.org
 
 cd /opt/mysql.javanile.org
 
