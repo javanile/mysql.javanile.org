71327a853ee382b285d0700efc6b816bbe3e119f
francesco <info.francescobianco@gmail.com>
2023-03-30 19:08:47 +0200

    Deploy

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+echo "$1" | sudo -S git config --global --add safe.directory /opt/mysql.javanile.org
+
 if [ ! -d /opt/mysql.javanile.org ]; then
    echo "$1" | sudo -S git config --global --add safe.directory /opt/mysql.javanile.org
    echo "$1" | sudo -S git clone https://github.com/javanile/mysql.javanile.org /opt/mysql.javanile.org
