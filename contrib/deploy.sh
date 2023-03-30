bc9501d85a7e8bc923e397826230e84c05ee6823
francesco <info.francescobianco@gmail.com>
2023-03-30 19:17:56 +0200

    Deploy

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "$1" | sudo -S apt-get install make
-
 if [ ! -d /opt/mysql.javanile.org ]; then
   echo "$1" | sudo -S apt-get install make
   echo "$1" | sudo -S git config --global --add safe.directory /opt/mysql.javanile.org
@@ -13,6 +11,10 @@ cd /opt/mysql.javanile.org
 
 echo "==> Update"
 echo "$1" | sudo -S git pull
+
+if [ ! -f .env ]; then
+  echo "$1" | sudo -S cp .env.prod .env
+fi
 
 echo "==> Restart"
 echo "$1" | sudo -S make restart
